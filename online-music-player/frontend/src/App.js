43807434c43ea8d8cd61a6c232a028bf0f544947
Adunolami <abinatitilayo2@gmail.com>
2024-07-25 15:21:33 +0100

    Create basic project structure

--- online-music-player/frontend/src/App.js
+++ online-music-player/frontend/src/App.js
@@ -1,27 +1,24 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Signin from './components/Signin';
-import Signup from './components/Signup';
-import { AuthProvider } from './context/AuthContext';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/signup" component={Signup} />
-          <Route path="/signin" component={Signin} />
-          <Route path="/" exact>
-            <h1>Welcome to the Online Music Player</h1>
-            <p><Link to="/signup">Sign Up</Link> | <Link to="/signin">Sign In</Link></p>
-          </Route>
-        </Switch>
-      </Router>
-    </AuthProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
