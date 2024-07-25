57891f722657a2d91d593542014bf3ef925cd95e
Adunolami <abinatitilayo2@gmail.com>
2024-07-25 11:01:59 +0100

    update

--- online-music-player/src/App.js
+++ online-music-player/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Home from './pages/Home';  // Create this later
+import Signup from './pages/Signup';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   );
 };
 
 export default App;
+
 
