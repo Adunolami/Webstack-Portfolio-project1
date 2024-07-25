19cf0dd27adce7958880bf54b5d934475f0edc0e
Adunolami <abinatitilayo2@gmail.com>
2024-07-25 12:37:49 +0100

    Mongos Server

--- online-music-player/backend/server/config/config.js
+++ online-music-player/backend/server/config/config.js
@@ -1,5 +1,7 @@
 require('dotenv').config(); // Load environment variables
 
+console.log('Loaded environment variables:', process.env);
+
 module.exports = {
   mongoURI: process.env.MONGODB_URI
 };
