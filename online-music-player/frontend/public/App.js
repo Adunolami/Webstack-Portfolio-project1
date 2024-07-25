96425f549e9db4d7391434b3c4bae8e4c471cc70
Adunolami <abinatitilayo2@gmail.com>
2024-07-25 15:45:12 +0100

    Authentication state

---
+++ online-music-player/frontend/public/App.js
@@ -0,0 +1,23 @@
+// App.js
+import React, { useState } from 'react';
+import Login from './Login';
+import Signup from './Signup';
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <div>
+      {!user ? (
+        <>
+          <Signup setUser={setUser} />
+          <Login setUser={setUser} />
+        </>
+      ) : (
+        <div>Welcome, {user.email}</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
