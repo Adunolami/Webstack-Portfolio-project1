1bfa97173661ae41259a4888ed79c453d41f1ef4
Adunolami <abinatitilayo2@gmail.com>
2024-07-25 14:16:59 +0100

    update signin component

--- online-music-player/frontend/src/components/Signin.js
+++ online-music-player/frontend/src/components/Signin.js
@@ -13,11 +13,26 @@ const Signin = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-      <button type="submit">Sign In</button>
-    </form>
+    <div>
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
   );
 };
 
