8ab73a82b84f344fed66773b6f021830899df7b4
Adunolami <abinatitilayo2@gmail.com>
2024-07-25 15:49:13 +0100

    All is up to date

--- online-music-player/backend/server/config/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config(); // Load environment variables
-
-console.log('Loaded environment variables:', process.env);
-
-module.exports = {
-  mongoURI: process.env.MONGODB_URI
-};
--- online-music-player/backend/server/routes/auth.js
+++ online-music-player/backend/server/routes/auth.js
@@ -1,37 +1,35 @@
-// backend/server/routes/auth.js
+// server/routes/auth.js
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-// Register
-router.post('/register', async (req, res) => {
+router.post('/signup', async (req, res) => {
   const { email, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
-    res.status(201).json({ message: 'User registered successfully!' });
+    const token = jwt.sign({ userId: newUser._id }, 'YOUR_SECRET_KEY');
+    res.json({ token, user: newUser });
   } catch (error) {
-    res.status(400).json({ message: 'Error registering user', error });
+    res.status(500).json({ error: 'Signup failed' });
   }
 });
 
-// Login
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
-    const token = jwt.sign({ id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
-    res.json({ token });
+    const token = jwt.sign({ userId: user._id }, 'YOUR_SECRET_KEY');
+    res.json({ token, user });
   } catch (error) {
-    res.status(400).json({ message: 'Error logging in', error });
+    res.status(500).json({ error: 'Login failed' });
   }
 });
 
 module.exports = router;
-
