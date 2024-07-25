e6b52325e967b96a6c90520f413d6d75ff721d15
Adunolami <abinatitilayo2@gmail.com>
2024-07-25 17:01:21 +0100

    app

--- online-music-player/frontend/src/App.js
+++ online-music-player/frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </AuthProvider>
+    </Router>
   );
 }
 
