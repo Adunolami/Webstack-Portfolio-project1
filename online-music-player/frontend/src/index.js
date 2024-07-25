d46f96ac71f3c57e5ce1e46cc1df1965d7467220
Adunolami <abinatitilayo2@gmail.com>
2024-07-25 14:50:34 +0100

    indexupdated

--- online-music-player/frontend/src/index.js
+++ online-music-player/frontend/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css'; // Adjust this path if your CSS file is located elsewhere
+import './index.css'; // 
 
 ReactDOM.render(
   <React.StrictMode>
