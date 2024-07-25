a8b6470c77d2bba4f836e67542c4b4cd37ffde76
Adunolami <abinatitilayo2@gmail.com>
2024-07-25 10:17:17 +0100

    Handle login logic and generate JWT tokens

---
+++ online-music-player/backend/server/controllers/authController.js
@@ -0,0 +1,18 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+  res.json({ token });
+};
+
+module.exports = { loginUser };
+
