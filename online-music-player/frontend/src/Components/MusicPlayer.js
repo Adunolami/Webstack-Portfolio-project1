52e4df3a91d283abb5ce929ec32c8900aa6b94a0
Adunolami <abinatitilayo2@gmail.com>
2024-07-26 00:55:36 +0100

    update

---
+++ online-music-player/frontend/src/Components/MusicPlayer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MusicPlayer = () => {
+  // Music player logic and UI here
+  return (
+    <div>
+      <h2>Music Player</h2>
+      {/* Add music player controls and UI */}
+    </div>
+  );
+};
+
+export default MusicPlayer;
