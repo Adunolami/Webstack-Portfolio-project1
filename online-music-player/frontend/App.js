7c4b101853a71f1aecd79c373740ce2544641862
Adunolami <abinatitilayo2@gmail.com>
2024-07-26 01:10:49 +0100

    App

--- online-music-player/frontend/App.js
+++ online-music-player/frontend/App.js
@@ -1,4 +1,22 @@
-// App.js
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  );
+}
+
+export default App;
+
+/*// App.js
 import React, { useState } from 'react';
 import Login from './Login';
 import Signup from './Signup';
@@ -21,3 +39,4 @@ function App() {
 }
 
 export default App;
+*/
